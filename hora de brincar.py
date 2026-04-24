efd3600fdef1ebcbb967fcb5c0075543a9c7952b
Williamzao12 <willamzao12@gmail.com>
2026-04-24 10:41:03 -0300

    Add files via upload

---
+++ hora de brincar.py
@@ -0,0 +1,13 @@
+import turtle
+from math import pi
+import random
+
+def quadrado():
+    turtle.speed(10000000000000000)
+    turtle.bgcolor("purple")
+    for i in range(50000000000000000000000000000):
+        turtle.forward(999)
+        turtle.right(1499)
+        turtle.right(pi)
+        
+quadrado()
